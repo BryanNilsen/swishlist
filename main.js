5953940dca157bbc17ec1be464eee15cb7bc8906
BryanNilsen <bryannilsen@gmail.com>
2018-11-21 14:43:48 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,132 @@
+const inspirationalQuotes = [
+  {
+    id: 0,
+    quote: "Don't give up. Don't ever give up.",
+    by: "Jim Valvano"
+  },
+  {
+    id: 1,
+    quote: "Shooting percentage is just as much about decision making as it is about technique.",
+    by: "Jeff Van Gundy"
+  },
+  {
+    id: 2,
+    quote: "All the successful teams I’ve ever seen have three characteristics: They play unselfish, they play together, and they play hard.",
+    by: "Larry Bird"
+  },
+  {
+    id: 3,
+    quote: "Everyone is born with a certain potential. You may never achieve your full potential, but how close you come depends on how much you want to pay the price.",
+    by: "Red Auerbach"
+  },
+  {
+    id: 4,
+    quote: "If you are afraid of failure you don’t deserve to be successful!",
+    by: "Charles Barkley"
+  },
+  {
+    id: 5,
+    quote: "I’ve got a theory that if you give 100 percent all of the time, somehow things will work out in the end.",
+    by: "Larry Bird"
+  },
+  {
+    id: 6,
+    quote: "Ask not what your teammates can do for you. Ask what you can do for your teammates.",
+    by: "Magic Johnson"
+  },
+  {
+    id: 7,
+    quote: "Obstacles don't have to stop you. If you run into a wall, don't turn around and give up. Figure out how to climb it, go through it, or work around it.",
+    by: "Michael Jordan"
+  },
+  {
+    id: 8,
+    quote: "Some people want it to happen, some wish it would happen, others make it happen.",
+    by: "Michael Jordan"
+  },
+  {
+    id: 9,
+    quote: "You can't get much done in life if you only work on the days when you feel good.",
+    by: "Jerry West"
+  },
+  {
+    id: 10,
+    quote: "I've missed more than 9000 shots in my career. I've lost almost 300 games. Twenty six times, I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.",
+    by: "Michael Jordan"
+  },
+  {
+    id: 11,
+    quote: "I can accept failure, everyone fails at something. But I can’t accept not trying.",
+    by: "Michael Jordan"
+  },
+  {
+    id: 12,
+    quote: "Nothing is given. Everything is earned.",
+    by: "LeBron James"
+  },
+  {
+    id: 13,
+    quote: "What do you do with a mistake: recognize it, admit it, learn from it, forget it.",
+    by: "Dean Smith"
+  },
+  {
+    id: 14,
+    quote: "Concentration and mental toughness are the margins of victory.",
+    by: "Bill Russell"
+  },
+  {
+    id: 15,
+    quote: "Basketball is a beautiful game when the five players on the court play with one heartbeat.",
+    by: "Dean Smith"
+  },
+  {
+    id: 16,
+    quote: "How good can we expect to be if our best player is not our best teammate.",
+    by: "Brad Stevens"
+  },
+  {
+    id: 17,
+    quote: "Leadership is diving for a loose ball. It’s about being able to take it as well as dish it out. That’s the only way you're going to get respect from other players.",
+    by: "Larry Bird"
+  },
+  {
+    id: 18,
+    quote: "You must expect great things of yourself before you can do them.",
+    by: "Michael Jordan"
+  },
+  {
+    id: 19,
+    quote: "Good players want to be coached. Great players want to be told the truth.",
+    by: "Doc Rivers"
+  },
+  {
+    id: 20,
+    quote: "Good, better, best. Never let it rest. Until your good is better and your better is best.",
+    by: "Tim Duncan"
+  },
+  {
+    id: 21,
+    quote: "If you quit ONCE, it becomes a habit. Never quit!",
+    by: "Michael Jordan"
+  },
+  {
+    id: 22,
+    quote: "There’s only one thing we can control, and that is how hard we play.",
+    by: "Don Meyer"
+  },
+  {
+    id: 23,
+    quote: "When you think you have done enough, do a little more, because someone out there is working harder than you.",
+    by: "Larry Bird"
+  },
+  {
+    id: 24,
+    quote: "I'm chasing perfection.",
+    by: "Kobe Bryant"
+  },
+  {
+    id: 25,
+    quote: "If every basketball player worked as hard as I did, I’d be out of a job.",
+    by: "Steve Nash"
+  },
+]
